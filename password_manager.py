c98144214288646ff00492d3d395a474f20d38f7
waithium <160033656+waithium@users.noreply.github.com>
2024-03-21 05:35:49 +0530

    Update password_manager.py
    ~added encryption of password in the password.txt file
    ~removed the master password functionality

--- password_manager.py
+++ password_manager.py
@@ -1,20 +1,41 @@
 # password manager
+from cryptography.fernet import Fernet
 
-master_pswd = input('Please enter your master password: ')
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+        
+write_key()
+'''
+
+
+def load_key():
+    file = open("password_manager\key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+# master_pswd = input('Please enter your master password: ')
+key = load_key() # + master_pswd.encode()
+fer = Fernet(key)
+
 
 def view():
-    with open('password.txt', 'r') as f:
+    with open('password_manager\password.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
-            user, paswd = data.split(' | ')
-            print('Username: ' + user + ' | Password: ' + paswd)
+            user, pswd = data.split(' | ')
+            print('Username: ' + user + ' | Password: ' + str(fer.decrypt(pswd.encode()).decode()))
 
 
 def add():
     name = input('enter your username: ')
     pswd = input('enter your password: ')
     with open('password.txt', 'a') as f:
-        f.write(name + ' | ' + pswd + '\n')
+        f.write(name + ' | ' + fer.encrypt(pswd.encode()).decode() + '\n')
 
 
 while True:
